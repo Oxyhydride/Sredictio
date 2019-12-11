19b0f9ac5c44de8d1e17ad61bc472eb3bcde612b
Ryan-Kan <kan_onn_kit@s2017.sst.edu.sg>
2019-12-11 23:01:52 +0800

    Update 1576076441
    - Updated `GUIMain.py` with more testing code.
    - Updated `Stock History.csv` with 2019-12-11 field test results.

--- GUI Test/GUIMain.py
+++ GUI Test/GUIMain.py
@@ -8,11 +8,56 @@ Copyright Ryan Kan 2019
 
 Description: The main python file for Sredictio's GUI
 
-NOTE: This is just testing.
+NOTE: This is just a test file.
 """
 # IMPORTS
 from tkinter import *
 from tkinter import ttk
+import tkinter.font as tk_font
+
+# CONSTANTS
+ERROR_STR_LEN = 50
+ERROR_CODES = {
+    1: "Not all entries are filled!",  # This is a common error!
+    2: "Stock Name is too short!",  # I wonder how this can get triggered...
+}
+
+
+# FUNCTIONS
+def set_error(error_code: int, error_str_len: int = ERROR_STR_LEN):
+    """
+    Places the error on the screen.
+
+    Keyword arguments:
+    - error_code, int: The error code. The program will extract the error from the above dictionary.
+    - error_str_len, int: The maximum size the error string can be.
+    """
+    outputLabel["text"] = f"ERROR {error_code:02d}: {ERROR_CODES[error_code]}".center(error_str_len, " ")
+    outputLabel["foreground"] = "Red"
+
+
+def compute():
+    # Reset output label colour
+    outputLabel["foreground"] = "Black"  # Set to black first
+
+    # Get entered variables
+    stock_name = stockName.get() if stockName.get() != "" else None
+    stock_symbol = stockSymbol.get() if stockSymbol.get() != "" else None
+
+    # Check if both variables are filled in
+    if stock_symbol is not None and stock_name is not None:
+        print(stock_name, stock_symbol)
+
+    else:
+        # Place an error on the window
+        set_error(1)
+        return
+
+    # Check stock name length
+    if len(stock_name) < 3:  # How can a stock's name be less than 3 characters?
+        set_error(2)
+        return
+
 
 # CODE
 # Create root window
@@ -20,7 +65,7 @@ root = Tk()
 root.title("Sredictio")
 
 # Fill in main window with the frame
-mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe = ttk.Frame(root, padding="3 4 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
@@ -29,11 +74,31 @@ root.rowconfigure(0, weight=1)
 stockName = StringVar()
 stockSymbol = StringVar()
 
-# Create entries
-stockNameEntry = ttk.Entry()
-stockNameEntry.grid(column=1, row=1, sticky=(W, E))
+# Create widgets
+stockNameLabel = ttk.Label(mainframe, text="Enter the Stock Name:", )
+stockNameLabel.grid(column=0, row=0, sticky=(W, E))
 
+stockNameEntry = ttk.Entry(mainframe, textvariable=stockName)
+stockNameEntry.grid(column=2, row=0, sticky=(W, E))
+
+stockSymbolLabel = ttk.Label(mainframe, text="Enter the Stock Symbol:")
+stockSymbolLabel.grid(column=0, row=1, sticky=(W, E))
+
+stockSymbolEntry = ttk.Entry(mainframe, textvariable=stockSymbol)
+stockSymbolEntry.grid(column=2, row=1, sticky=(W, E))
+
+outputLabel = ttk.Label(mainframe, font=tk_font.Font(family="Courier New", size=16))
+outputLabel.grid(column=1, row=3, sticky=W)
+outputLabel["text"] = " " * ERROR_STR_LEN
+
+ttk.Button(mainframe, text="Compute!", command=compute).grid(column=1, row=2)
+
+# Configure widgets to be placed on grid
 for child in mainframe.winfo_children():
-    child.grid_configure(padx=5, pady=5)
+    child.grid_configure(padx=3, pady=4)
 
+# Bind the Return key to the compute function
+root.bind('<Return>', compute)
+
+# Run the application
 root.mainloop()
