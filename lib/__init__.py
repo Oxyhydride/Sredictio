3df11fdde90f2f70fe92d56502b3da874767498f
Ryan-Kan <25820201+Ryan-Kan@users.noreply.github.com>
2019-12-12 23:28:57 +0800

    Update 1576164489
    Started updating files to be 0.2.0 compatible.
    - `__init__.py` was incorrectly formatted. Fixed that.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,11 +1,8 @@
 """
-__init__.py.py
-Version <INT>.<INT>.<INT>
+__init__.py
 
 Created on 2019-12-10
-Updated on YYYY-MM-DD
+Updated on 2019-12-10
 
 Copyright Ryan Kan 2019
-
-Description: <TEXT>
 """
